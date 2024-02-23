ebbc6612eb05ac8f412f180ef3b827d60a8f157a
Makhi7 <mvmangxola22@gmail.com>
2024-02-23 12:03:43 +0200

    01-Create-Next-Ecommerce-App-Layout

---
+++ components/header/header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Maphiko Amazon
+          </Link>
+          <ul className="flex">
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/cart">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className="btn btn-ghost rounded-btn" href="/signin">
+                Sign in
+              </Link>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
